cecf3fac1b4f0a059ebce9efb3c9ace624d53dc6
Арман Аланян <armanalanyan007@gmail.com>
2026-06-10 21:20:00 +0300

    ds

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,33 +1,27 @@
 import { Post, PostsService } from './posts.service';
 
-
 describe('PostsService', () => {
   let postsService: PostsService;
   const post: Omit<Post, 'id' | 'date'> = {
     text: 'Mocked post',
   };
 
-
   beforeEach(async () => {
     postsService = new PostsService();
-
 
     postsService.create({ text: 'Some pre-existing post' });
   });
 
-
   it('should add a new post', () => {
-    postsService.create(post);
-    const allPosts = postsService.getAll();
-    expect(allPosts.length).toBe(2); // был один "pre-existing", плюс новый
-    expect(allPosts[1].text).toBe(post.text);
-  });
+    const createdPost = postsService.create(post);
 
+    expect(postsService.find(createdPost.id)).toEqual(createdPost);
+  });
 
   it('should find a post', () => {
     const createdPost = postsService.create(post);
-    const foundPost = postsService.findById(createdPost.id);
-    expect(foundPost).toEqual(createdPost);
-    expect(foundPost.text).toBe(post.text);
+    const foundPost = postsService.find(createdPost.id);
+
+    expect(foundPost).toMatchObject(post);
   });
 });
